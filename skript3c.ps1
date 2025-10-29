1e8dfdfaddfee9d9bd6baf06192dbbbcddcbde0d
Memokre <147373468+Memokre@users.noreply.github.com>
2025-10-29 15:20:46 +0100

    Update skript3c.ps1

--- skript3c.ps1
+++ skript3c.ps1
@@ -1,1 +1,15 @@
 
+Get-LocalUser | Where-Object { -not $_.LastLogon } | Select-Object Name, Description, LastLogon
+
+
+Get-CimInstance -ClassName Win32_UserAccount -Filter "LocalAccount = True" | Where-Object { -not $_.LastLogon } | Select-Object Name, Caption, LocalAccount
+
+
+
+
+
+
+
+Get-LocalUser | Where-Object { $_.AccountIsLockedOut -eq $True } | Select-Object Name, AccountIsLockedOut, LastLogon
+
+Get-CimInstance -ClassName Win32_UserAccount -Filter "Lockout = True AND LocalAccount = True" | Select-Object Name, Caption, Lockout
