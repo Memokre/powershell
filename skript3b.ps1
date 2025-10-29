ee44d4db650f6d73f0faa3a3b57c5b64934b0124
Memokre <147373468+Memokre@users.noreply.github.com>
2025-10-29 15:19:28 +0100

    Update skript3b.ps1

--- skript3b.ps1
+++ skript3b.ps1
@@ -1,1 +1,25 @@
+$DriveLetter = "C:"
+$NewLabel = "System"
 
+$Volume = Get-CimInstance -ClassName Win32_Volume -Filter "DriveLetter='$DriveLetter'"
+
+if ($Volume) {
+    if ($Volume.Label -ne $NewLabel) {
+        Set-CimInstance -InputObject $Volume -Property @{Label=$NewLabel}
+        
+        $NewVolume = Get-CimInstance -ClassName Win32_Volume -Filter "DriveLetter='$DriveLetter'"
+    } else {
+        "Disk $DriveLetter: již má požadovaný název '$NewLabel'. Přeskakuji přejmenování."
+    }
+} else {
+    "Chyba: Disk s písmenem $DriveLetter: nebyl nalezen."
+}
+
+
+
+cim only solution
+
+(Get-CimInstance -ClassName Win32_Volume -Filter "DriveLetter='C:'").Label
+
+$Volume = Get-CimInstance -ClassName Win32_Volume -Filter "DriveLetter='C:'"
+Set-CimInstance -InputObject $Volume -Property @{Label='System'}
